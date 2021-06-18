d87010dfd0908c166f8251b4d95567505d4a0e69
Elias Sims <59635261+elisims@users.noreply.github.com>
2021-06-18 14:56:57 -0600

    Add files via upload

---
+++ lab3_pt1/Scheme.h
@@ -0,0 +1,18 @@
+/*
+
+Elias Sims, Section 1, elisims@byu.edu
+Lab 3: Relational Database
+
+*/
+
+#ifndef SCHEME_H_
+#define SCHEME_H_
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Scheme: public vector<string> { };
+
+#endif /* SCHEME_H_ */
