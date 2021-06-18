0c20ce2d2bfe038f36a872977768d5b5cf0ed65f
Elias Sims <59635261+elisims@users.noreply.github.com>
2021-06-18 14:55:08 -0600

    Add files via upload

---
+++ lab2/Token.cpp
@@ -0,0 +1,27 @@
+/*
+
+Elias Sims, Section 1, elisims@byu.edu
+Lab 1: Lexical Analyzer
+
+*/
+
+#include <sstream>
+
+#include "Token.h"
+
+using namespace std;
+
+/*Simple toString() function used to print out the final output in main.cpp*/
+string Token::toString() {
+	stringstream ss;
+	ss << '(' << identifier << ",\"" << content << "\"," << lineNumber << ')' << '\n';
+	return ss.str();
+}
+
+bool Token::isValid(string identifier) {
+	if (identifier == this->identifier) {
+		return true;
+	}
+
+	return false;
+}
